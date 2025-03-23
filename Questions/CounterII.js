063b9b974c09f3cd4df2c82016d36b8049284f3a
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-03-23 21:56:38 +0530

    CounterII [LeetCode-2665]

---
+++ Questions/CounterII.js
@@ -0,0 +1,23 @@
+//Problem Link-https://leetcode.com/problems/counter-ii/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val=init;
+    const obj={increment :()=> ++val,
+        decrement :()=> --val,
+        reset : function(){
+            val=init
+            return val
+        }}
+    return obj;
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
